e28c4361537dee233da0e9bc62c1c1022184fa78
bokade <121025630+bokade@users.noreply.github.com>
2024-07-05 12:44:22 +0530

    my second commit

---
+++ src/main/java/com/example/practice/MyNewController.java
@@ -0,0 +1,15 @@
+package com.example.practice;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping
+public class MyNewController {
+
+    @GetMapping("/hii")
+    public String getString(){
+        return "Hello how are you?";
+    }
+}
